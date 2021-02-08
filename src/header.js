04f9cd73085d0e772f33283378a6ee18b1c69c46
Kat Sauma <katsauma@gmail.com>
2021-02-08 15:08:04 -0800

    image assets

---
+++ src/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class OurCoolerHeader extends React.Component {
+    render() {
+        return (
+            <header>
+                <ul>
+                    <
+                </ul>
+            </header>
+        );
+    }
+}
